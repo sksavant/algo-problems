04be312c75069f0c8028e55794308b018c3578f3
Savant Krishna <savant.2020@gmail.com>
2013-10-21 09:42:59 +0530

    latlong regex prob

---
+++ hackerrank/regex-practice/latlong.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import re
+n = int(raw_input())
+latlongregex = re.compile('\([\-\+]*\d+[.\d]*, [+-]*\d+.*\d*\)')
+lat1regex = re.compile('(?<=\()[\+\-]*[1-9]\d+.\d+(?=,)')
+lat2regex = re.compile('(?<=\()[\+\-]*[1-9]\d+(?=,)')
+lng1regex = re.compile('(?<=, )[\+\-]*[1-9]\d+.\d+(?=\))')
+lng2regex = re.compile('(?<=, )[\+\-]*[1-9]\d+(?=\))')
+for i in range(n):
+    x = raw_input()
+    if not latlongregex.match(x):
+        print "Invalid"
+        #print "latlong"
+        continue
+    if lng1regex.findall(x)==[] and lng2regex.findall(x)==[]:
+        #print lng1regex.match(x), lng2regex.match(x)
+        #print lng1regex.findall(x), lng2regex.findall(x)
+        print "Invalid"
+        #print "long"
+        continue
+    if lat1regex.findall(x)==[] and lat2regex.findall(x)==[]:
+        print "Invalid"
+        #print "lat"
+        continue
+    lat,lng = map(float, x.strip('() +').split(', '))
+    try:
+        assert lat <= 90 and lat>=-90
+    except AssertionError:
+        print "Invalid"
+        continue
+    try:
+        assert lng <= 180 and lng>=-180
+    except AssertionError:
+        print "Invalid"
+        continue
+    #print lat,lng
+    print "Valid"
